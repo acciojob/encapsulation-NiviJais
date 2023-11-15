63625b974e1051119be1a65ebbd9bf8a406f480c
Nivi0284 <nivijaiswal45@gmail.com>
2023-11-15 20:11:11 +0530

    Encapsulation using code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+
+//        obj.name = "john";
+//        System.out.println(obj.name);
+        //you might seen some error[
+
+        // set the value using setter
+        obj.setName("John");
+        // Access the value using getter
+        System.out.println(obj.getName());
+
+    }
 }
